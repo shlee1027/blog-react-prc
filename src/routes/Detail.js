eecb32c41fd57579915ea02708c6d2484149fb61
Sanghyun Lee <shlee1027@daum.net>
2023-01-16 23:19:21 +0900

    탭 ui만들기위해  bootstrap에서 가져옴

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
+import { Nav } from "react-bootstrap";
 
 function Detail(props) {
   useEffect(() => {
@@ -35,6 +36,21 @@ function Detail(props) {
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
+
+      <Nav variant="tabs" defaultActiveKey="link0">
+        <Nav.Item>
+          <Nav.Link eventKey="link0">버튼0</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link1">버튼1</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link2">버튼2</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <div>내용0</div>
+      <div>내용1</div>
+      <div>내용2</div>
     </div>
   );
 }
